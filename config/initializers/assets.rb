32a3ff6fa85fe805c04290380980426463f0264f
omnivor <lukasz.targanski@gmail.com>
2018-10-09 16:18:05 +0200

    scaffold posts

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -8,6 +8,18 @@ Rails.application.config.assets.version = '1.0'
 # Add Yarn node_modules folder to the asset load path.
 Rails.application.config.assets.paths << Rails.root.join('node_modules')
 
+Rails.application.config.assets.precompile += %w( font-awesome.min.css )
+Rails.application.config.assets.precompile += %w( main.css )
+Rails.application.config.assets.precompile += %w( noscript.css )
+
+Rails.application.config.assets.precompile += %w( jquery.min.js )
+Rails.application.config.assets.precompile += %w( jquery.scrollex.min.js )
+Rails.application.config.assets.precompile += %w( jquery.scrolly.min.js )
+Rails.application.config.assets.precompile += %w( browser.min.js )
+Rails.application.config.assets.precompile += %w( breakpoints.min.js )
+Rails.application.config.assets.precompile += %w( util.js )
+Rails.application.config.assets.precompile += %w( main.js )
+
 # Precompile additional assets.
 # application.js, application.css, and all non-JS/CSS in the app/assets
 # folder are already added.
