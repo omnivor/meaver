723712f2a70d2c5bdc432d7bdfb0d215bce48290
omnivor <lukasz.targanski@gmail.com>
2018-10-10 13:04:58 +0200

    Setup contact form

--- db/schema.rb
+++ db/schema.rb
@@ -10,12 +10,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_10_09_140041) do
+ActiveRecord::Schema.define(version: 2018_10_09_143124) do
 
   create_table "posts", force: :cascade do |t|
     t.string "description"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "image_file_name"
+    t.string "image_content_type"
+    t.bigint "image_file_size"
+    t.datetime "image_updated_at"
   end
 
 end
